058368ace7d0a4282ba12646ac31608e5ffa5f59
Ramoif <Ramoif>
2021-10-31 11:51:01 +0800

    10.31v3 main test

--- main.c
+++ main.c
@@ -2,7 +2,19 @@
 
 void method1();
 
+#define r 2
+#define l 3
+
 int main() {
+
+
+
+    return 0;
+}
+
+//测试helloworld
+void method1() {
+    printf("方法测试1");
     printf("这是我的第一个项目。\n");
     printf("Hello, World!\n");
     int a[3];
@@ -15,9 +27,16 @@ int main() {
         printf("a[%d] = %d\n", i, a[i]);
     }
     method1();
-    return 0;
 }
 
-void method1() {
-    printf("方法测试1");
+//测试二维数组
+void method2() {
+    int a[r][l] = {0};
+    for (int i = 0; i < r; ++i) {
+        for (int j = 0; j < l; ++j) {
+            printf("%d ",a[i][j]);
+        }
+        printf("\n");
+    }
+    printf("打印r行l列数组全部内容↑。");
 }
