2a24bfaf86dab201f11ef03eb72a3e3fcc3dd1a5
Ramoif <Ramoif>
2021-12-24 14:06:00 +0800

    12.24v1 define test

--- Demo01/demo11.c
+++ Demo01/demo11.c
@@ -1,10 +1,17 @@
 #include "stdio.h"
 
+#define MAX(a, b) ((a)>(b)?(a):(b))
+
 int fibonacii(int fi);
 
 /*斐波那契数列a1+a2=a3*/
 
 int main() {
+    //测试宏函数
+    int t1 = 23, t2 = 34, t3 = 12;
+    int max = MAX(MAX(t1, t2), t3);
+    printf("最大值是%d\n", max);
+
     int a = 1, b = 1, c = 0, sum = 0;
     sum = a + b;
     for (int i = 0; i < 18; i++) {
@@ -16,7 +23,7 @@ int main() {
     printf("前20项的合为：%d", sum);
 
     for (int j = 0; j < 20; ++j) {
-        printf("第%d个斐波那契数为 : %d\n", j+1, fibonacii(j));
+        printf("第%d个斐波那契数为 : %d\n", j + 1, fibonacii(j));
     }
 
 
